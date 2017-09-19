917f6cdfb3097dccec5066ead1294e568e932143
Connlaoi Smith <connlaoi.smith@dcmail.ca>
2017-09-19 14:35:27 -0400

    Changes to Tri Changes

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-
 $title = "Index";
+
 $author = "Minh Tri, Ly";
+$group = "Group 21";
+
 $date = "June 12th";
 $filename = "index.php";
 $banner = "Index Page";
-$description = "This Page will serve as the hompage";
+$description = "This Page will serve as the homepage for deliverable one.";
 ?>
 <?php include 'header.php'; ?>
 
 
-	   <p>It was created as course requirement for webd2201, this is my first times try to code a web with the course code <a href ="../../pufferd/webd2201/"> 
-	WEBD 2201 </a> </p>
+	   <p>It was created as course requirement for webd3201, this is my first times try to code a web with the course code <a href ="../../pufferd/webd2201/"> 
+	WEBD 3201 </a> </p>
 	   <p>and the website <a href ="http://www.durhamcollege.ca"> Durham College </a></p>
 	   <p><a href="http://www.durhamcollege.ca"> <img src="images/dc_fullcolour.jpg" alt="DC Logo" height="200" width="400"/> </a>  </p>
 	
