624260f6de8e09f5ea5ccde0c5e995c2d7aedf29
Connlaoi Smith <connlaoi.smith@dcmail.ca>
2017-09-19 14:23:50 -0400

    Minor tweak to functions.php

--- group21/includes/functions.php
+++ group21/includes/functions.php
@@ -7,7 +7,7 @@
 	$updateddate = "xxxx xx 2017";
 
 	// display the footer copyright information
-	function displayCopyrightInfo() {
+	function display_copyright() {
 		// save copyright year and group name to string for output
 		$footer_info = "&copy; " . date("Y") . " Group 21"; 
 		return $footer_info;
