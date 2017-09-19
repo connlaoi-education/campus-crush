bd3d88db1749a526b3a69a9780f6287d25bb97a0
Minh Tri Ly <minhtri.ly@dcmail.ca>
2017-09-19 14:30:42 -0400

    Try to update index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$title = "Index";
+$author = "Minh Tri, Ly";
+$date = "June 12th";
+$filename = "index.php";
+$banner = "Index Page";
+$description = "This Page will serve as the hompage";
+?>
+<?php include 'header.php'; ?>
+
+
+	   <p>It was created as course requirement for webd2201, this is my first times try to code a web with the course code <a href ="../../pufferd/webd2201/"> 
+	WEBD 2201 </a> </p>
+	   <p>and the website <a href ="http://www.durhamcollege.ca"> Durham College </a></p>
+	   <p><a href="http://www.durhamcollege.ca"> <img src="images/dc_fullcolour.jpg" alt="DC Logo" height="200" width="400"/> </a>  </p>
+	
+<?php include 'footer.php'; ?>
