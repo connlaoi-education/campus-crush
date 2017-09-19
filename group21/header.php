d0d6a5743ad53c5dec9888264f60df0549b84a1c
Connlaoi Smith <connlaoi.smith@dcmail.ca>
2017-09-19 16:11:10 -0400

    Tweaked files

--- group21/header.php
+++ group21/header.php
@@ -14,7 +14,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-    <link rel="stylesheet" href="http://opentech2.durhamcollege.org/webd3201/group21/css/crushstyle.css" type="text/css" />
+    <link rel="stylesheet" href="http://opentech2.durhamcollege.org/webd3201/group21/css/crush.css" type="text/css" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>
     <?php
