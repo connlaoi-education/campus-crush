c5ff306867812f73149d642fee24e01003e77203
Minh Tri Ly <minhtri.ly@dcmail.ca>
2017-09-25 22:54:57 -0400

    Static pages

---
+++ group21/profile-search.php
@@ -0,0 +1,32 @@
+<!-- Lab Info Variables -->
+<?php
+		$title = "Campus Crush - Login";
+		$createddate = "September 19 2017";
+		$updateddate = "xxxx xx 2017";
+		$filename = "user-login.php";
+		$banner = "Welcome back to Campus Crush!";
+		$description = "We need a description here....";
+?>
+<!--
+		Creator:      Connlaoi Smith
+        Filename:    <?php echo $filename; ?>
+        Created:      <?php echo $createddate; ?>
+        Updated:     <?php echo $updateddate; ?>
+        Description: <?php echo $description; ?>
+-->
+
+<!-- Include Header PHP -->
+<?php include 'header.php'; ?>
+
+<!-- HTML -->
+<span class="titleh3">Description</span>
+<p class="content"><?php echo $description; ?></p>
+
+<br />
+<hr />
+<br />
+
+
+
+<!-- Include Footer PHP -->
+ <?php include 'footer.php'; ?>
