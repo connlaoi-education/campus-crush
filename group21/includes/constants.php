aff5998badfb2e00a9ac8a86629bde70ccd185dd
subzero9998 <reaper9998@hotmail.com>
2017-09-28 03:04:10 -0400

    fixing constants and db files with .gitignore

---
+++ group21/includes/constants.php
@@ -0,0 +1,34 @@
+<?php
+	// FILE FOR DEFINED CONSTANTS
+	$filename = "constants.php";
+	$authors = "Connlaoi";
+	
+	$createddate = "September 19 2017";
+	$updateddate = "xxxx xx 2017";
+
+	
+	// define the lengths of values for validation
+	define("MINIMUM_ID_LENGTH", 5, false);
+	define("MAXIMUM_ID_LENGTH", 20, false);
+	define("MINIMUM_PASSWORD_LENGTH", 6, false);
+	define("MAXIMUM_PASSWORD_LENGTH", 15, false);
+	define("MAXIMUM_FIRST_NAME_LENGTH", 20, false);
+	define("MAXIMUM_LAST_NAME_LENGTH", 30, true);
+	define("MAXIMUM_EMAIL_LENGTH", 255, true);
+	
+	// define the user types
+	define("ADMIN", 'a', true);
+	define("CLIENT", 'c', true);
+	define("INCOMPLETE", 'i', true);
+	define("DISABLED", 'd', true);
+	
+	// define database connector values for BOTH local and server for easy swapping
+	define("DATABASE_HOST_LOCAL", 'localhost:8443', true);
+	define("DATABASE_HOST_SERVER", 'opentech2.durhamcollege.org:/var/www/html/webd3201/group21', true);
+	// define group database name
+	define("DATABASE_NAME", 'group21_db', true);
+	// define admin user and password
+	define("DATABASE_USER", 'group21_admin', true);
+	define("DATABASE_PASSWORD", 'password', false);
+	
+?>
---
+++ group21/includes/db.php
@@ -0,0 +1,16 @@
+<?php
+	// FILE FOR DATABASE CONNECTIVITY
+	$filename = "db.php";
+	$authors = "Connlaoi";
+	
+	$createddate = "September 19 2017";
+	$updateddate = "xxxx xx 2017";
+	
+	// open a database connection
+	function db_connect(){	
+		// set the connection values
+		// to change the host switch between DATABASE_HOST_SERVER and DATABASE_HOST_LOCAL
+		$conn = pg_connect("host=". DATABASE_HOST_SERVER . " dbname=" . DATABASE_NAME . " user=" . DATABASE_USER . " password=" . DATABASE_PASSWORD);
+		return $conn;
+	}
+?>
