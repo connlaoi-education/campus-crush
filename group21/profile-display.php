cdfbdaf90fd4d3040e29d12db8dcfb4232497319
subzero9998 <reaper9998@hotmail.com>
2017-09-28 15:55:28 -0400

    changed lorem ipsum text to pull from API

--- group21/profile-display.php
+++ group21/profile-display.php
@@ -23,18 +23,11 @@
 
 <br />
 <hr />
-<p class="content">Try-hard swag yr cloud bread. Skateboard you probably haven't heard of them taiyaki, pickled cardigan snackwave crucifix church-key heirloom swag man braid organic sustainable ugh roof party. Iceland enamel pin locavore fixie. Actually hot chicken pinterest, mustache gochujang mixtape green juice cloud bread umami chillwave dreamcatcher lyft. Unicorn everyday carry typewriter, +1 glossier polaroid cardigan mumblecore umami. Salvia mlkshk leggings vexillologist, roof party authentic craft beer snackwave farm-to-table affogato pitchfork normcore wayfarers. Freegan mlkshk pour-over meggings affogato normcore. Live-edge yuccie hammock copper mug tousled blue bottle brunch chartreuse coloring book fam. Seitan vexillologist jean shorts four loko adaptogen kinfolk fixie cronut brunch. Live-edge readymade cliche, green juice lumbersexual kitsch bitters salvia everyday carry viral photo booth polaroid kale chips echo park art party. Aesthetic shoreditch copper mug vape. Slow-carb literally hella, jean shorts scenester irony sartorial activated charcoal deep v kale chips etsy flexitarian leggings thundercats. Poutine gentrify blue bottle tote bag jean shorts lomo tbh shoreditch fixie pop-up narwhal plaid fam. VHS plaid tattooed pitchfork bicycle rights edison bulb sustainable fashion axe williamsburg.</p>
-<br />
-<br />
-<p class="content">Ugh tumblr authentic, dreamcatcher banh mi craft beer narwhal viral thundercats 8-bit. Fingerstache food truck schlitz keffiyeh vexillologist truffaut subway tile stumptown gochujang. Adaptogen activated charcoal raw denim master cleanse. Master cleanse glossier art party, letterpress cred neutra DIY pug poke kitsch small batch etsy. Disrupt kitsch live-edge flannel shaman. Meh squid seitan man braid hashtag sustainable gluten-free. Sustainable adaptogen leggings, slow-carb heirloom thundercats vegan gochujang trust fund flannel art party tbh truffaut. Echo park tumeric synth shoreditch air plant taiyaki. Trust fund marfa kinfolk gochujang enamel pin unicorn, swag small batch craft beer. Hashtag tousled tbh, coloring book sriracha 8-bit vegan photo booth pug normcore ethical listicle portland. Paleo chartreuse godard, pok pok chambray woke dreamcatcher ugh before they sold out marfa gastropub. Activated charcoal humblebrag keffiyeh four loko microdosing fixie squid hashtag, air plant poke schlitz fingerstache 90's photo booth VHS. Selfies +1 before they sold out deep v messenger bag leggings skateboard. Portland actually banjo disrupt, celiac sriracha swag lyft cliche godard pour-over edison bulb.</p>
-<br />
-<br />
-<p class="content">Asymmetrical master cleanse literally, thundercats kogi drinking vinegar lomo pinterest cold-pressed activated charcoal. Narwhal next level man bun, activated charcoal paleo wayfarers organic. Narwhal post-ironic flexitarian four loko cornhole affogato venmo hell of chambray green juice offal scenester franzen street art. Kitsch twee tumblr williamsburg. Hammock tousled four loko ethical portland normcore, tbh truffaut. Af mumblecore live-edge irony organic brunch artisan. Retro green juice VHS ugh bespoke street art quinoa keytar godard plaid PBR&B blue bottle thundercats. Fingerstache neutra four loko, slow-carb pickled unicorn jianbing. Butcher organic pabst cronut, mixtape ramps snackwave meditation kombucha actually normcore venmo celiac enamel pin. Health goth jean shorts cold-pressed pug literally. Hashtag franzen skateboard man bun, pinterest mumblecore hexagon kitsch intelligentsia bushwick stumptown umami.</p>
-<br />
-<br />
-<p class="content">Cold-pressed scenester fam, crucifix hell of williamsburg slow-carb yuccie butcher tattooed snackwave tofu prism biodiesel. Organic vaporware taxidermy food truck fingerstache plaid viral. Four dollar toast woke tacos, thundercats shoreditch man braid slow-carb farm-to-table bicycle rights viral. Shaman aesthetic polaroid marfa tote bag austin tbh echo park edison bulb skateboard biodiesel tacos tattooed. Blog subway tile knausgaard taiyaki, etsy pok pok unicorn. Glossier gastropub sriracha poutine ugh. Chartreuse everyday carry paleo, neutra pug blog flexitarian sustainable keffiyeh. Thundercats YOLO sartorial food truck pinterest gastropub. Woke pickled ramps, vexillologist kogi trust fund disrupt meh chia lyft.</p>
 
-<br />
+<?php
+$content = file_get_contents('http://loripsum.net/api');
+echo("<p>" . $content . "</p>");
+?>
 
 
 
