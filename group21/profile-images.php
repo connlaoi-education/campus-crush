b65d9a6e1095a9b809d3e9320bdcab14434af1c2
subzero9998 <reaper9998@hotmail.com>
2017-10-20 01:05:39 -0400

    added images, pass request and update pages

---
+++ group21/profile-images.php
@@ -0,0 +1,21 @@
+<!-- Page Info -->
+<?php
+    $title = "Campus Crush - Register";
+    $createddate = "September 19 2017";
+    $updateddate = "xxxx xx 2017";
+    $filename = "user-register.php";
+    $banner = "Campus Crush - Registration";
+    $description = "Sign up quickly and easily - start meeting new people now!";
+?>
+<!--
+  Creator:      Jeremy Power, Minh Tri Ly
+  Filename:    <?php echo $filename; ?>
+  Created:      <?php echo $createddate; ?>
+  Updated:     <?php echo $updateddate; ?>
+  Description: <?php echo $description; ?>
+-->
+
+<!-- Include Header PHP -->
+<?php include 'header.php'; ?>
+
+<?php include 'footer.php'; ?>
