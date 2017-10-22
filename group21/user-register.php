252c6723bb3422a4cd11a0686b39cb19508a88e0
subzero9998 <reaper9998@hotmail.com>
2017-10-22 05:34:14 -0400

    fixed registration SQL birthday

--- group21/user-register.php
+++ group21/user-register.php
@@ -197,10 +197,10 @@ if(isLoggedIn()) {
 			{	
 				$today = date("Y-m-d", time());
 				$connection = db_connect();
-
+				$birthdate = date("Y-m-d", ($year . "-" . $month . "-" . $day));
 				$results = pg_prepare($connection, "insert_user", 'INSERT INTO users (id, password, first_name, last_name, email_address, account_type, birthday, enroll_date, last_access) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)');
 				$results = pg_execute($connection, "insert_user", array($username, md5($password), $first_name,
-				$last_name, $email, $account_type, date_create($year . "-" . $month . "-" . $day), $today, $today));
+				$last_name, $email, $account_type, $birthdate, $today, $today));
 	      $_SESSION['register'] = "Registration successful, please try login";
 				header("Location:user-login.php");
 				ob_flush();
