1e07b6433207aa52b8b64d6346ed03251364f9da
subzero9998 <reaper9998@hotmail.com>
2017-10-23 01:56:53 -0400

    fixed profile update bug

--- group21/includes/db.php
+++ group21/includes/db.php
@@ -28,7 +28,7 @@
 
 	pg_prepare($connection, "insert_profile", 'INSERT INTO profiles (user_id, gender, gender_sought, city, image, headline, self_description, match_description, relationship_sought, relationship_status, preferred_age_minimum, preferred_age_maximum, religion_sought, education_experience, race, habit, exercise, residence_type, campus) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)');
 
-	pg_prepare($connection, "update_profile", "UPDATE profiles SET gender = $1, gender_sought = $2, city = $3, image = $4, headline = $5, self_description = $6, match_description = $7, relationship_sought = $8, relationship_status = $9, preferred_age_minimum = $10, preferred_age_maximum = $11, religion_sought = $12, education_experience = $13, race = $14, habit = $15, exercise = $16, residence_type = $17, campus = $18");
+	pg_prepare($connection, "update_profile", "UPDATE profiles SET gender = $1, gender_sought = $2, city = $3, image = $4, headline = $5, self_description = $6, match_description = $7, relationship_sought = $8, relationship_status = $9, preferred_age_minimum = $10, preferred_age_maximum = $11, religion_sought = $12, education_experience = $13, race = $14, habit = $15, exercise = $16, residence_type = $17, campus = $18 WHERE user_id = $19");
 
 
 	pg_prepare($connection, "update_account", 'UPDATE users SET account_type = $1 WHERE id = $2');
