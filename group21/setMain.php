930026f4f9ef45be4d3aa033a81b86563e96f610
Connlaoi Smith <connlaoi.smith@dcmail.ca>
2017-12-21 03:36:56 -0500

    additional files for functionality questionable db.php file(?)

---
+++ group21/setMain.php
@@ -0,0 +1,19 @@
+<?php
+
+  session_start();
+  require('includes/constants.php');
+  require('includes/db.php');
+  require('includes/functions.php');
+
+
+  if(!isset($_GET['main']))
+  {
+    header("Location: dashboard.php");
+  }
+
+$connection = db_connect();
+$results = pg_execute($connection, "update_user_default_image", array($_GET['mainImage'], $_SESSION['username']));
+
+
+header("Location:profile-images.php");
+?>
