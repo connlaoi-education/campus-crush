bc6f3504631306b9c038308ad84e77edd0ea9f96
Connlaoi <connlaoi@gmail.com>
2018-01-08 20:03:59 -0500

    Initial Commit Prior to starting work

--- group21/tester.php
+++ group21/tester.php
@@ -76545,7 +76545,7 @@ function randomPassword($length) {
 
 function generateUsers() {
 	// iterate through 1000 users
-	for ($user = 0; $user < 2500; $user++) 
+	for ($user = 0; $user < 1000; $user++) 
 	{
 
 	// GENERATE USER INFORMATION
