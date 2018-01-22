b2ebcdf6b22d5f0c410e259a2f07e3c8943d3411
Connlaoi <connlaoi@gmail.com>
2018-01-21 20:49:25 -0500

    Fixing git files

--- group21/email.php
+++
@@ -1,7 +0,0 @@
-<html><body>
-  <img src=http://opentech2.durhamcollege.org/webd3201/group21/images/cc_logo.png alt='Campus Crush' />
-  <h2>Greetings, Jeremy!</h2>
-  <h3>Your password on Campus Crush has been changed to: g2J592hD</h3>
-  <h3>This change occured on: <?php echo(date('Y-m-d H:i:s')); ?></h3>
-  <h3>If you have any problems contact us at: group21@opentech2.durhamcollege.org</h3>
-</body></html>
